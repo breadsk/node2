7cb56a880d253b7cf757938db9bcf72c751ade30
Nicolas <nicolas.programador@gmail.com>
2026-01-20 20:16:17 -0300

    metodos para actualizar

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ const Curso = mongoose.model('Curso', cursoSchema);
 
 const crearCurso = async() => {
     const curso = new Curso({
-        nombre      : 'Curso de Java',
-        autor       : 'Nicolás Cáceres',
-        etiquetas   : ['Java', 'poo'],
+        nombre      : 'Ingenieria de Software',
+        autor       : 'Juan Cartez',
+        etiquetas   : ['Isf', 'Arquitectura'],
         publicado   : true
     });
     const resultado = await curso.save();
@@ -54,15 +54,50 @@ const listarCursos = async() => {
     //     .sort({autor : 1})//1 ascendente , -1 descendente
     //     .select({nombre : 1, etiquetas : 1});//Con 1 las muestra
     // console.log(cursos);
+    const numeroPage = 2;
+    const sizePage = 10;
+    // api/cursos?numeroPage=4&sizePage=10
     const cursos = await Curso
         //.find({nombre : 'Curso de Java'})
         //.find({autor: /^Nic/ })
-        .find({autor: /res$/})//termina        
-        //.find({autor: /.*col.*/})//cuando un campo tiene un contenido especifico
+        //.find({autor: /res$/})//termina        
+        ////.find({autor: /.*col.*/})//cuando un campo tiene un contenido especifico
+        .find({autor: /^Nic/ })
+        .skip((numeroPage - 1) * sizePage)        
         .limit(10)
         .sort({autor : 1})//1 ascendente , -1 descendente
         .select({nombre : 1, etiquetas : 1, autor: 1});//Con 1 las muestra
     console.log(cursos);
 }
 
-listarCursos();
+//listarCursos();
+
+//Con esto me devuelve el documento pero antes de ser actualizado
+const actualizarCurso = async(id) => {
+    const resultado = await Curso.findByIdAndUpdate(id,{
+        $set: {
+            autor:'Nicolas Caceres',
+            publicado: true
+        }
+    },{ new: true });//con new:true me devuelve el documento actualizado
+    console.log(resultado);      
+}
+
+// const actualizarCurso = async(id) => {
+//     const curso = await Curso.findById(id);
+//     if(!curso){
+//         console.log('El curso no existe');
+//         return;
+//     }
+//     curso.publicado = false;
+//     curso.autor = 'nikin';
+//     // curso.set({
+//     //     publicado : false,
+//     //     autor: 'draculin'
+//     // });
+
+//     const resultado = await curso.save();
+//     console.log(resultado);
+// }
+
+actualizarCurso('697006ef27e80e662782b295');
